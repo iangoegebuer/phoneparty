005753a883ad99a210dcd4a1ada1bfa48dae5703
ChrisLHall <mrchrislhall@gmail.com>
2019-01-07 21:10:41 -0800

    URL parsing function (untested)

---
+++ site/js/util.js
@@ -0,0 +1,12 @@
+function getURLVars() {
+  var url = location.href.split("?");
+  if (url.length < 2) return;
+  var query = url[1];
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var items = part.split("=");
+    if (items.length < 2) return;
+    result[decodeURIComponent(items[0])] = decodeURIComponent(items[1]);
+  });
+  return result;
+}
